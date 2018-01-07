81529dda410643c1036f63756f294c1df22c027a
jaouad ballat <jaouad.ballat@gmail.com>
2018-01-07 11:08:06 +0000

    Set up the front end page

--- vendor/tcg/voyager/resources/views/master.blade.php
+++ vendor/tcg/voyager/resources/views/master.blade.php
@@ -10,7 +10,9 @@
 
     <!-- Favicon -->
     <link rel="shortcut icon" href="{{ voyager_asset('images/logo-icon.png') }}" type="image/x-icon">
-
+     
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 
 
     <!-- App CSS -->
