77563cb9458e8179e5155c54a853145887470932
jaouad ballat <jaouad.ballat@gmail.com>
2018-01-07 12:07:33 +0000

    Set up one to many category-company relationship

--- app/Category.php
+++ app/Category.php
@@ -7,5 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    
+    public function companies()
+    {
+    	return $this->hasMany('App\Company');
+    }
 }
--- app/Company.php
+++ app/Company.php
@@ -7,5 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Company extends Model
 {
-    
+    public function category()
+    {
+    	return $this->belongsTo('App\Category');
+    }
 }
